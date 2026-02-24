6f2c1f6eefe42bb9968212e6c5df143a6f90cd31
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-24 09:30:25 -0300

    commit da aula 1

---
+++ bin/aula01.dart
@@ -0,0 +1,38 @@
+void main() {
+  //Vars
+  // var nome = 'Bob';
+  // String profissao = 'Engenheiro de Software';
+
+  // Scape
+  // var msg = "It's easy to learn dart";
+
+  // String linguagem = 'Dart';
+  // print('Você está aprendendo $linguagem. é muito diveritdo!');
+
+  // int a = 5;
+  // int b = 3;
+  // print('A soma de $a e $b é ${a + b}.');
+  // String nome2 = 'Diego';
+  // print('O seu nome em maiusculas é ${nome2.toUpperCase()}.');
+
+  final List<String> linguagens = ['Dart', 'Java', 'Python'];
+  linguagens.add('PHP');
+  // print(linguagens);
+  // print(linguagens[1]);
+  // print(linguagens.length);
+  // print(linguagens.contains("Dart"));
+  // for(var i = 0; i < linguagens.length; i++) {
+  //   print(linguagens[3]);
+  // }
+
+  // for (String lingua in linguagens) {
+  //   print(lingua);
+  // }
+   for (String linguagem in linguagens){
+   print linguagem;
+  }
+}
+
+ void printarValor(String valor) {
+   print(valor);
+ }
